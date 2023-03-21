d070f97d22057c6c440696e3fcaba23054ddc596
EdmundXie <609031809@qq.com>
2023-03-21 18:23:54 +0000

    add search results for Document

--- src/test/java/cn/itcast/hotel/HotelSearchTest.java
+++ src/test/java/cn/itcast/hotel/HotelSearchTest.java
@@ -3,24 +3,32 @@ package cn.itcast.hotel;
 import cn.itcast.hotel.pojo.HotelDoc;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.lucene.queryparser.xml.builders.BooleanQueryBuilder;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Projectname: hotel-demo
@@ -33,6 +41,35 @@ import java.util.List;
 public class HotelSearchTest {
 
     private RestHighLevelClient client;
+
+    //测试highlight
+    @Test
+    public void testHighlight() throws IOException {
+        //1.准备request对象
+        SearchRequest request = new SearchRequest("hotel");
+        //2.设置request对象
+        request.source().query(QueryBuilders.matchQuery("all","如家")).size(100);
+        request.source().highlighter(new HighlightBuilder().field("name").requireFieldMatch(false));
+        //3.发送request
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        handleResponse(response);
+    }
+
+    //测试pageAndsort
+    @Test
+    public void testPageAndSort() throws IOException {
+        //1.准备request对象
+        SearchRequest request = new SearchRequest("hotel");
+        //2.设置request对象
+        request.source().query(QueryBuilders.matchQuery("all","如家")).size(100);
+        request.source().sort("price", SortOrder.ASC);
+        request.source().from(0).size(10);
+        //3.发送request
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        handleResponse(response);
+    }
 
     //测试bool
     @Test
@@ -85,22 +122,39 @@ public class HotelSearchTest {
         SearchHit[] searchHits = hits.getHits();
 
         List<HotelDoc> hotelDocList = new ArrayList<>();
+        System.out.println("total docs: "+total+"\n");
         //取得source的json数据
         for(SearchHit hit:searchHits){
             String json = hit.getSourceAsString();
+//            System.out.println(json);
             HotelDoc hotelDoc = JSON.parseObject(json,HotelDoc.class);
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if(!CollectionUtils.isEmpty(highlightFields)){
+                HighlightField highlightField = highlightFields.get("name");
+                if(highlightField!=null){
+                    String name = highlightField.getFragments()[0].toString();
+                    hotelDoc.setName(name);
+                }
+            }
+            System.out.println(hotelDoc);
             hotelDocList.add(hotelDoc);
         }
-
-//        打印结果
-        System.out.println("total docs: "+total);
-        System.out.println(hotelDocList.get(1)+" \n"+hotelDocList.get(2));
     }
 
     @BeforeEach
     public void setUp(){
         this.client = new RestHighLevelClient(RestClient.builder(
-                HttpHost.create("http://124.222.138.40:9200")
+                new HttpHost("124.222.138.40", 9200))
+                        .setRequestConfigCallback(
+                        new RestClientBuilder.RequestConfigCallback() {
+                            @Override
+                            public RequestConfig.Builder customizeRequestConfig(
+                                    RequestConfig.Builder requestConfigBuilder) {
+                                return requestConfigBuilder
+                                        .setConnectTimeout(5000)
+                                        .setSocketTimeout(60000);
+                            }
+                        }
         ));
     }
 
